e03def75484529b3b34770ad015c8029329edbd7
CSC2017 <jc25202@email.vccs.edu>
2017-04-13 20:54:40 -0400

    add MATH2.java

---
+++ MATH2.java
@@ -0,0 +1,19 @@
+
+public class MATH2 {
+
+		public static void main(String[] args) {
+			
+			System.out.println(Math.pow(2, 4));
+			System.out.println(Math.sqrt(100));
+			System.out.println(Math.abs(-100));
+			System.out.println(Math.random());
+			System.out.println(Math.max(99.9, 22));
+			System.out.println(Math.min(10,100));
+			System.out.println(Math.round(5.967));
+			System.out.println(Math.ceil(8.5));
+			System.out.println(Math.floor(8.5));
+
+
+		}
+}
+
